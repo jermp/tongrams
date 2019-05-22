61dff3f005ed6768d76e23d16a7a43f8f6698aab
Nadav Benedek <nadavb@users.noreply.github.com>
2019-05-22 11:46:16 +0300

    bug fixed, handler returned as long

--- python/tongrammodule.cpp
+++ python/tongrammodule.cpp
@@ -9,31 +9,36 @@ using namespace tongrams;
 using namespace std;
 
 
+// https://docs.python.org/3/extending/newtypes_tutorial.html
+
 static PyObject * tongram_load(PyObject *self, PyObject *args){
     const char *binary_filename;
-    pef_trie_PSEF_ranks_count_lm * model_p = new pef_trie_PSEF_ranks_count_lm;
+    pef_trie_PSEF_ranks_count_lm * model_p;
 
+    model_p = new pef_trie_PSEF_ranks_count_lm;
     if (!PyArg_ParseTuple(args, "s", &binary_filename)) return NULL;
 
     auto model_string_type = util::get_model_type(binary_filename);
 
-	std::cout << "tongram model_string_type: " << model_string_type << "\n";
-	util::logger("Loading data structure");
-	size_t file_size = util::load(*model_p, binary_filename);
+    std::cout << "tongram model_string_type: " << model_string_type << "\n";
+    util::logger("Loading data structure");
+    size_t file_size = util::load(*model_p, binary_filename);
 
-	std::cout << "\tTotal bytes: " << file_size << "\n";
-	std::cout << "\tTotal ngrams: " << model_p->size() << "\n";
-	std::cout << "\tBytes per gram: " << double(file_size) / model_p->size() << std::endl;
+    std::cout << "\tTotal bytes: " << file_size << "\n";
+    std::cout << "\tTotal ngrams: " << model_p->size() << "\n";
+    std::cout << "\tBytes per gram: " << double(file_size) / model_p->size() << std::endl;
 
-    // return Py_BuildValue("i", 0); // Integet
-    return Py_BuildValue("O", model_p); // Any kind of object
+    return Py_BuildValue("k", model_p); // unsigned long. https://docs.python.org/3/c-api/arg.html
 }
+
 
 static PyObject * tongram_lookup(PyObject *self, PyObject *args){
     const char *ngrams_space_separated;
     pef_trie_PSEF_ranks_count_lm *pointer_to_model;
 
-    if (!PyArg_ParseTuple(args, "Os", &pointer_to_model, &ngrams_space_separated))  return NULL; // https://docs.python.org/3/extending/extending.html
+    unsigned long pointer_as_long;
+    if (!PyArg_ParseTuple(args, "ls", &pointer_as_long, &ngrams_space_separated))  return NULL; // https://docs.python.org/3/extending/extending.html
+    pointer_to_model = (pef_trie_PSEF_ranks_count_lm *)pointer_as_long;
 
     stl_string_adaptor adaptor;
 
@@ -42,7 +47,7 @@ static PyObject * tongram_lookup(PyObject *self, PyObject *args){
     // cout << "lookup value: " << value1 << std::endl;
 
 	if (value1 == global::not_found) {
-	    return Py_BuildValue("K", 0); // unsigned long long
+	    return Py_BuildValue("K", 0); // unsigned long long // https://docs.python.org/3/c-api/arg.html
 	} else {
 	    return Py_BuildValue("K", value1); // unsigned long long
 	}
