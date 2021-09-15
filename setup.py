ac634ae593d10f59ed91154c3a9ca69a7056335a
KOLANICH <kolan_n@mail.ru>
2021-09-15 14:06:34 +0300

    Moved the python packaging files into the root dir.

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from distutils.core import Extension
 module = Extension('tongrams',
                     define_macros = [('MAJOR_VERSION', '1'),
                                      ('MINOR_VERSION', '0')],
-                    include_dirs = ['../include', '../external/emphf', '../external/essentials'],
+                    include_dirs = ['./include', './external/emphf', './external/essentials'],
                     libraries = ['boost_iostreams', 'boost_regex'],
                     extra_compile_args=['-std=c++17'],
-                    sources = ['tongrams.cpp'])
+                    sources = ['python/tongrams.cpp'])
 
 setup(ext_modules = [module])
